4ecbefdec94e83ae91ce3ed6fb355c79db61a724
Dave Nadeau <37254473+DaveNadeau@users.noreply.github.com>
2021-04-25 16:21:03 -0700

    implemented basic relay logic

--- watering_system.c
+++ watering_system.c
@@ -5,78 +5,171 @@
 
 //global variables
 #define QTY_HYGROS 2
+#define QTY_RELAYS 2
+#define QTY_ZONES 2
 
 //pin assignments
 #define HYGRO_POWER_PIN 2
 #define DIGITAL_HYGRO_PIN1 16
 #define DIGITAL_HYGRO_PIN2 17
 
+#define RELAY1 18
+#define RELAY2 19
+
+//array of hygrometers
+int hygrometers[QTY_HYGROS] = {DIGITAL_HYGRO_PIN1, DIGITAL_HYGRO_PIN2};
+
+//array of relays
+int relays[QTY_RELAYS] = {RELAY1, RELAY2};
+
+//struct for each garden zone & array created
+struct garden_zone
+{
+    bool hygro_reading;
+    bool relay_power;
+} garden_zones[QTY_ZONES];
+
+//function declarations
+void init_hygros();
+void init_relays();
+void record_hygros();
+void run_water_cycle();
+
+//test functions
+void test_relay();
+
 int main()
 {
     stdio_init_all();
-    printf("indoor herb garden v 1.0\n");
+    printf("Indoor herb garden v 1.0\n");
 
-    // adc_init();
-
-    // // Make sure GPIO is high-impedance, no pullups etc
-    // adc_gpio_init(26);
-    // // Select ADC input 0 (GPIO26)
-    // adc_select_input(0);
-
-    //intialized digital pins to power and read hygrometer
-    gpio_init(HYGRO_POWER_PIN);
-    gpio_init(DIGITAL_HYGRO_PIN1);
-    gpio_init(DIGITAL_HYGRO_PIN2);
-
-    // set pin to output power
-    gpio_set_dir(HYGRO_POWER_PIN, GPIO_OUT);
-
-    //set pin to be read (so as an input) and pull down for 0 reading (not sure if needed) for device 1
-    gpio_set_dir(DIGITAL_HYGRO_PIN1, GPIO_IN);
-    gpio_pull_down(DIGITAL_HYGRO_PIN1);
-
-    //set pin to be read (so as an input) and pull down for 0 reading (not sure if needed) for device 2
-    gpio_set_dir(DIGITAL_HYGRO_PIN2, GPIO_IN);
-    gpio_pull_down(DIGITAL_HYGRO_PIN2);
+    //initialize components
+    init_hygros();
+    init_relays();
 
     while (1)
     {
-        // // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
-        // const float conversion_factor = 3.3f / (1 << 12);
-        // uint16_t result = adc_read();
-        // printf("Raw value: %u, voltage: %f V\n", result, result * conversion_factor);
-
         //power up the hygrometers and let them stablize
         gpio_put(HYGRO_POWER_PIN, 1);
         sleep_ms(200);
 
-        //digital read 1
-        bool digital_result1 = gpio_get(DIGITAL_HYGRO_PIN1);
-        if (!digital_result1)
-        {
-            printf("Unit 1 Digital value: %d (water okay)\n", digital_result1);
-        }
-        else
-        {
-            printf("Unit 1 Digital value: %d (needs water)\n", digital_result1);
-        }
-
-        //digital read 2
-        bool digital_result2 = gpio_get(DIGITAL_HYGRO_PIN2);
-        if (!digital_result2)
-        {
-            printf("Unit 2 Digital value: %d (water okay)\n", digital_result2);
-        }
-        else
-        {
-            printf("Unit 2 Digital value: %d (needs water)\n", digital_result2);
-        }
+        //read and save hygro values into garden zone struct
+        record_hygros();
 
         //power down hygro until next reading
         gpio_put(HYGRO_POWER_PIN, 0);
 
-        //based on stored value from hygro, start pump and open required solenoids
+        //water if necessary
+        run_water_cycle();
 
-        sleep_ms(10000);
+        //sleep until next reading
+        sleep_ms(15000);
     }
 }
+
+void init_hygros()
+{
+    //init power pin
+    gpio_init(HYGRO_POWER_PIN);
+    gpio_set_dir(HYGRO_POWER_PIN, GPIO_OUT);
+
+    //init digital read pins on hygros
+    for (int i = 0; i < QTY_HYGROS; ++i)
+    {
+        gpio_init(hygrometers[i]);
+        gpio_set_dir(hygrometers[i], GPIO_IN);
+        gpio_pull_down(hygrometers[i]);
+    }
+}
+
+void init_relays()
+{
+    for (int i = 0; i < QTY_RELAYS; ++i)
+    {
+        gpio_init(relays[i]);
+        gpio_set_dir(relays[i], GPIO_OUT);
+        gpio_put(relays[i], 0);
+    }
+}
+
+void record_hygros()
+{
+    for (int i = 0; i < QTY_HYGROS; ++i)
+    {
+        bool digital_reading = gpio_get(hygrometers[i]);
+        garden_zones[i].hygro_reading = digital_reading;
+    }
+}
+
+void run_water_cycle()
+{
+    bool needs_water = false;
+    for (int i = 0; i < QTY_ZONES; ++i)
+    {
+        if (garden_zones[i].hygro_reading)
+        {
+            gpio_put(relays[i], 1);
+            needs_water = true;
+            printf("Watering zone %u\n", i);
+        }
+        else
+        {
+            gpio_put(relays[i], 0);
+            printf("Zone %u okay\n", i);
+        }
+    }
+
+    // if one of the readings says it needs water, then turn on the pump
+    // if(needs_water){
+
+    // }
+    sleep_ms(7000);
+
+    // turn off power and close relays
+    for (int i = 0; i < QTY_ZONES; ++i)
+    {
+        gpio_put(relays[i], 0);
+    }
+}
+
+//test function implementations
+// void test_relay()
+// {
+//     //testing relay
+//     gpio_init(RELAY1);
+//     gpio_set_dir(RELAY1, GPIO_OUT);
+//     gpio_put(RELAY1, 0);
+//     sleep_ms(2000);
+//     gpio_put(RELAY1, 1);
+// }
+
+// void read_hygros()
+// {
+//     //test function... should set zone value for processing later.
+//     for (int i = 0; i < QTY_HYGROS; ++i)
+//     {
+//         bool digital_reading = gpio_get(hygrometers[i]);
+//         if (!digital_reading)
+//         {
+//             printf("Unit %u Digital value: %d (water okay)\n", i, digital_reading);
+//         }
+//         else
+//         {
+//             printf("Unit %u Digital value: %d (needs water)\n", i, digital_reading);
+//         }
+//     }
+// }
+
+// void set_garden_zones()
+// {
+//     for (int i = 0; i < QTY_ZONES; ++i)
+//     {
+//         garden_zones[i].hygro_reading = hygrometers[i];
+//     }
+//     for (int i = 0; i < QTY_ZONES; i++)
+//     {
+//         if (garden_zones[i].hygro_reading)
+//         {
+//         }
+//     }
+// }
